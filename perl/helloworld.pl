9647cd7a15dd2510ae9734c0fe5748f17d531906
Alex Merced <alex@alexmerced.dev>
2021-02-18 12:14:18 -0500

    update raku/perl

---
+++ perl/helloworld.pl
@@ -0,0 +1,11 @@
+print "Hello, world\n";
+
+$hello = "Cheese";
+
+$para = <<"cheese";
+I love $hello
+this is me doing stuff in perl
+this works!
+cheese
+
+print("$para \n")
---
+++ ruby/other/ruby3.rb
@@ -0,0 +1,3 @@
+user = { name: 'Oliver', age: 29, role: 'CTO' }
+
+p user.except(:role)
